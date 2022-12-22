7abfb1b2567e2ca69ac883627be4177353eebe80
yoonseo0812 <113665407+202120383cys@users.noreply.github.com>
2022-12-23 03:02:48 +0900

    fixfixfixfix

--- works/work1/Sketch.js
+++ works/work1/Sketch.js
@@ -1,34 +1,30 @@
 function setup() {
-  let boundingRects = document
-    .getElementById("p5Canvas")
-    .getBoundingClientRect();
-  let canvas = createCanvas(boundingRects.width, boundingRects.height);
-  canvas.parent("p5Canvas");
+  createCanvas(400, 400);
   angleMode(DEGREES);
 }
 
 function draw() {
   background(0);
-  translate(300, 300);
+  translate(200, 200);
   rotate(-90);
 
   let hr = hour();
   let mn = minute();
   let sc = second();
 
-  strokeWeight(15);
+  strokeWeight(8);
   stroke(255, 100, 150);
   noFill();
   let secondAngle = map(sc, 0, 60, 0, 360);
-  arc(0, 0, 500, 500, 0, secondAngle);
+  arc(0, 0, 300, 300, 0, secondAngle);
 
   stroke(150, 100, 255);
   let minuteAngle = map(mn, 0, 60, 0, 360);
-  arc(0, 0, 450, 450, 0, minuteAngle);
+  arc(0, 0, 280, 280, 0, minuteAngle);
 
   stroke(150, 255, 100);
   let hourAngle = map(hr % 12, 0, 12, 0, 360);
-  arc(0, 0, 400, 400, 0, hourAngle);
+  arc(0, 0, 260, 260, 0, hourAngle);
 
   stroke(255);
   point(0, 0);
